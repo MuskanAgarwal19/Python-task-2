0be870abd310e83da7da2c93fac2bc73885c6447
MuskanAgarwal19 <muskanagarwal19112004@gmail.com>
2024-06-25 20:40:25 +0530

    Add files via upload

---
+++ To_do_List.py
@@ -0,0 +1,52 @@
+# todo_list.py
+
+class ToDoList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+        print(f"Task '{task}' added to the list!")
+
+    def view_tasks(self):
+        if not self.tasks:
+            print("Your to-do list is empty!")
+        else:
+            print("Your to-do list:")
+            for i, task in enumerate(self.tasks, start=1):
+                print(f"{i}. {task}")
+
+    def delete_task(self, task_number):
+        try:
+            del self.tasks[task_number - 1]
+            print(f"Task {task_number} deleted!")
+        except IndexError:
+            print("Invalid task number!")
+
+def main():
+    todo = ToDoList()
+
+    while True:
+        print("\n1. Add task")
+        print("2. View tasks")
+        print("3. Delete task")
+        print("4. Quit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            task = input("Enter a task: ")
+            todo.add_task(task)
+        elif choice == "2":
+            todo.view_tasks()
+        elif choice == "3":
+            task_number = int(input("Enter the task number to delete: "))
+            todo.delete_task(task_number)
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again!")
+
+if __name__ == "__main__":
+    main()
